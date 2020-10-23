29bac8a44611a442587325cc4e0a51b3b008b6fb
Victor Hugo Alves <vgoncalves360@hotmail.com>
2020-10-23 10:21:38 -0300

    fix home redirect

---
+++ app/Http/Controllers/ResidenceController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Residence;
+use Illuminate\Http\Request;
+
+class ResidenceController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Residence  $residence
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Residence $residence)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Residence  $residence
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Residence $residence)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Residence  $residence
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Residence $residence)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Residence  $residence
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Residence $residence)
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('condo/index');
+    return redirect('admin/condos');
 });
 
 Auth::routes();
