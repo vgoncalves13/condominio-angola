bb95a169f1ffee293645f2b9a94522d060fe3739
Rodrigo Dias Sales <RodrigoDcontato@gmail.com>
2020-11-17 17:42:06 -0300

    Teste

--- database/factories/AddressFactory.php
+++ database/factories/AddressFactory.php
@@ -2,18 +2,18 @@
 
 namespace Database\Factories;
 
-use App\Models\Address;
 use App\Models\Condo;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
 
-class AddressFactory extends Factory
+class CondoFactory extends Factory
 {
     /**
      * The name of the factory's corresponding model.
      *
      * @var string
      */
-    protected $model = Address::class;
+    protected $model = Condo::class;
 
     /**
      * Define the model's default state.
@@ -23,12 +23,8 @@ class AddressFactory extends Factory
     public function definition()
     {
         return [
-            'condo_id' => Condo::factory(),
-            'address' => $this->faker->address,
-            'address2' => $this->faker->randomNumber(5),
-            'district' => $this->faker->city,
-            'city_id' => $this->faker->numberBetween(1,25),
-            'postal_code' => $this->faker->randomNumber(8),
+            'name' => $this->faker->name,
+            'email' => $this->faker->unique()->safeEmail,
         ];
     }
 }
